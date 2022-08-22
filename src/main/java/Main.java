3d5d3f679f05668f7b6f38f18cfb8e2ca08e149e
Maxim <vedernikovmaxim@rambler.ru>
2022-08-22 21:53:35 +0300

    solved simpli path tusk v1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,50 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-        boolean isFormatted = false;
-        String request = "пожалуйста форматируйте код";
-        // исправили теперь build succcessful
-        if (isFormatted) {
+        String path = "/home//gs/gh/..//hdd//";
+        String[] buf = path.split("/");
+        System.out.println(Arrays.toString(buf));
+        String ss = canonicalPath(path);
+        System.out.println(ss);
+    }
+    public static String canonicalPath(String path){
+        String[] buf = path.split("/");
+        StringBuilder buf_string = new StringBuilder("");
+        List<String> buf_list = new ArrayList<>();
+        Stack<String> stack_path = new Stack<>();
+        for (String word: buf){
+            if(!word.equals("") && !word.equals(" ") &&!word.equals(".") ){
+                if(word.equals("...")){
+                    if(!stack_path.empty()){
+                        stack_path.push(word);
+                        break;
+                    }
+                    else{
+                        return "/...";
+                    }
+                }
+                if(word.equals("..")){
+                    if(!stack_path.empty()){
+                       String rem = stack_path.pop();
+                    }
+                }
+                stack_path.push(word);
+            }
         }
+        if(stack_path.empty()){
+            return "/";
+        }
+        while (!stack_path.empty()){
+            buf_list.add(stack_path.pop());
+            buf_list.add("/");
+        }
+        for (int i = buf_list.size()-1; i >=0 ; i--) {
+            buf_string.append(buf_list.get(i));
+        }
+        return buf_string.toString();
     }
 }
