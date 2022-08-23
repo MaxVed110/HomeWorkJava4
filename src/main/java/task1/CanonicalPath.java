9d4bc4380e637ce2b33121ef7aa029a7bd526184
Maxim <vedernikovmaxim@rambler.ru>
2022-08-23 11:13:48 +0300

    checkstyle

--- src/main/java/task1/CanonicalPath.java
+++ src/main/java/task1/CanonicalPath.java
@@ -1,48 +1,44 @@
 package task1;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Stack;
 
 public class CanonicalPath {
     public static void main(String[] args) {
         String path = "/home//gs/gh/..//hdd//";
-        String[] buf = path.split("/");
-        System.out.println(Arrays.toString(buf));
         String ss = canonicalPath(path);
         System.out.println(ss);
     }
 
     public static String canonicalPath(String path) {
         String[] buf = path.split("/");
-        StringBuilder buf_string = new StringBuilder("");
-        Stack<String> stack_path = new Stack<>();
+        StringBuilder bufString = new StringBuilder("");
+        Stack<String> stackPath = new Stack<>();
         for (String word : buf) {
             if (!word.equals("") && !word.equals(" ") && !word.equals(".")) {
                 if (word.equals("...")) {
-                    if (!stack_path.empty()) {
-                        stack_path.push(word);
+                    if (!stackPath.empty()) {
+                        stackPath.push(word);
                         break;
                     } else {
                         return "/...";
                     }
                 }
                 if (word.equals("..")) {
-                    if (!stack_path.empty()) {
-                        String rem = stack_path.pop();
+                    if (!stackPath.empty()) {
+                        String rem = stackPath.pop();
                     }
                     continue;
                 }
-                stack_path.push(word);
+                stackPath.push(word);
             }
         }
-        if (stack_path.empty()) {
+        if (stackPath.empty()) {
             return "/";
         }
-        while (!stack_path.empty()) {
-            buf_string.insert(0, stack_path.pop());
-            buf_string.insert(0,"/");
+        while (!stackPath.empty()) {
+            bufString.insert(0, stackPath.pop());
+            bufString.insert(0, "/");
         }
-        return buf_string.toString();
+        return bufString.toString();
     }
 }
